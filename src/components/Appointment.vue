b1b5e4ada334ea1f747e5ae967748eb39e22e59a
Victor Reznichenko <vit.79@bk.ru>
2022-02-25 14:43:24 +0300

    complete dashboard

---
+++ src/components/Appointment.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card flat color="transparent" class="mt-n5">
+    <v-toolbar color="rgba(0,0,0,0)" flat class="py-4">
+      <v-toolbar-title>Appointment</v-toolbar-title>
+    </v-toolbar>
+    <polar-chart
+        :chart-id="chartId"
+        :width="width"
+        heigth="200"
+        :css-classes="cssClasses"
+        :styles="styles"
+        :plygins="plugins"
+    />
+  </v-card>
+</template>
+
+<script>
+import PolarChart from "@/chart/PolarChart";
+
+export default {
+  name:'AppointmentChart',
+  components: {
+    PolarChart
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: "polar-chart",
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 100,
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {
+      },
+    },
+    plugins: {
+      type: Array,
+      default: () => {
+      },
+    },
+  },
+}
+</script>
---
+++ src/components/History.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card flat color="transparent" class="mt-n5">
+    <v-toolbar color="rgba(0,0,0,0)" flat class="py-4">
+      <v-toolbar-title>History Survey</v-toolbar-title>
+    </v-toolbar>
+    <line-chart
+        :chart-id="chartId"
+        :width="width"
+        heigth="200"
+        :css-classes="cssClasses"
+        :styles="styles"
+        :plygins="plugins"
+    />
+  </v-card>
+</template>
+
+<script>
+import LineChart from "@/chart/LineChart";
+
+export default {
+  name:"HistoryChart",
+  components: {
+    LineChart
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: "line-chart",
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 100,
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {
+      },
+    },
+    plugins: {
+      type: Array,
+      default: () => {
+      },
+    },
+  },
+}
+</script>
